f3d2414beaadefcef51726a3aa4b2c83b258afa2
maknetaRo <makneta@gmail.com>
2020-08-04 13:01:38 +0200

    Updates

---
+++ precache-manifest.c5716e67479dd3d93d8b19ce4f9fd1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5046acc4ae56581744ac8923e45eb76",
+    "url": "/random-recipe/index.html"
+  },
+  {
+    "revision": "aba20fc3775153d1227b",
+    "url": "/random-recipe/static/css/main.6158bf9c.chunk.css"
+  },
+  {
+    "revision": "d4932ceb7e5c196b6173",
+    "url": "/random-recipe/static/js/2.e025f5f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-recipe/static/js/2.e025f5f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba20fc3775153d1227b",
+    "url": "/random-recipe/static/js/main.3345b621.chunk.js"
+  },
+  {
+    "revision": "c79dbda3aecad6006826",
+    "url": "/random-recipe/static/js/runtime-main.dd6b1878.js"
+  }
+]);
